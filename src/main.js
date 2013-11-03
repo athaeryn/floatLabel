894cd28c7ee24f1b8671977b023b070bc3288cbe
kwnccc <kostas2812@hotmail.com>
2013-11-04 01:24:30 +0200

    Added new behavior with a zoom effect. Optimize the way label is being used, by creating it. Better format and code organization for main.js. New behavior using previous label element changes.

--- src/main.js
+++ src/main.js
@@ -11,9 +11,11 @@
       this.el = el;
       this.options = options;
       this.input = this._findInput();
-      this.label = this._findLabel();
+      this.labelStr = this._findLabelStr();
+      this.effect = this._findEffect();
       this.namespace = options.namespace || 'fly';
       // Do things
+      this.label = this._createLabel(this._createLabelStr());
       this._bindEvents();
     }
 
@@ -21,8 +23,44 @@
       _findInput: function () {
         return $(this.el).find('input, textarea');
       },
-      _findLabel: function () {
-        return $(this.el).find('label');
+      _findLabelStr: function() {
+        return this.input.data('label');
+      },
+      _findEffect: function() {
+        return this.input.data('effect');
+      },
+      _createLabelStr : function() {
+        var effectLabelStr = ''; 
+        for(var i=0; i < this.labelStr.length; i++){
+
+          if(i%2 == 0){
+            var order ="even";
+          }else{
+            var order ="odd";
+          }
+
+          if(this.labelStr.substr(i, 1) == ' '){
+            this.labelStr.substr(i, 1).replace(/ /g,'\u00a0');
+            // i+=6;
+          }
+        
+          if(i == 0){
+            effectLabelStr += '<span class="'+order+' first">' + this.labelStr.substr(i, 1)+'</span>';
+          }else if(i+1 == this.labelStr.length){
+            effectLabelStr += '<span class="'+order+' last">' + this.labelStr.substr(i, 1) + '</span>';
+          }else{
+            effectLabelStr += '<span class="'+order+'">' + this.labelStr.substr(i, 1)+'</span>';
+          }
+
+        }
+
+        return effectLabelStr;
+      },
+      _createLabel: function (effectLabelStr) {
+        console.log(effectLabelStr);
+        var label = $('<label for="'+this.input.attr('name')+'" class="'+this.namespace+'__label '+this.namespace+'__label--'+this.effect+'">'+effectLabelStr+'</label>')
+        this.input.before(label);
+        return label;
       },
       _bindEvents: function () {
         this.input.on('keyup', $.proxy(this._onKeyUp, this));
